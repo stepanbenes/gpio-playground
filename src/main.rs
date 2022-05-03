ab5c0e01e3927bb035e336342e14c388fe2c25fa
Štěpán Beneš <benes.stepan@gmail.com>
2022-05-03 19:39:07 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("init echo is_high: {}", echo_pin.is_high());
 
-    echo_pin.set_async_interrupt(Trigger::Both, |level| println!("echo: {}", level))?;
+    echo_pin.set_async_interrupt(Trigger::RisingEdge, |level| println!("up echo: {}", level))?;
+    echo_pin.set_async_interrupt(Trigger::FallingEdge, |level| println!("down echo: {}", level))?;
 
     //trigger_pin.set_low();
     
     // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+    //let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
         
     // Sleep for 2 seconds while the LED blinks.
     thread::sleep(Duration::from_secs(10));
@@ -40,10 +41,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     thread::sleep(Duration::from_micros(10));
     trigger_pin.set_low();
 
-    for i in 0..2000 {
-        pwm.set_frequency(100.0, (i % 100) as f64 * 0.01f64)?;
-        thread::sleep(Duration::from_millis(10));
-    }
+    // for i in 0..2000 {
+    //     pwm.set_frequency(100.0, (i % 100) as f64 * 0.01f64)?;
+    //     thread::sleep(Duration::from_millis(10));
+    // }
 
 
 
