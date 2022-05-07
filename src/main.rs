1e5dfb2191e65f1ae225295dbbcc847c98b7577b
Štěpán Beneš <benes.stepan@gmail.com>
2022-05-07 12:46:53 +0200

    test motors

--- src/main.rs
+++ src/main.rs
@@ -38,68 +38,36 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let gpio = rppal::gpio::Gpio::new()?;
 
-    let mut trigger_pin = gpio.get(23)?.into_output();
-    let mut echo_pin = gpio.get(24)?.into_input();
+    let mut forward1_pin = gpio.get(22)?.into_output();
+    let mut backward1_pin = gpio.get(23)?.into_output();
+    let mut forward2_pin = gpio.get(24)?.into_output();
+    let mut backward2_pin = gpio.get(25)?.into_output();
 
-    trigger_pin.set_low();
+    forward1_pin.set_high();
+    backward1_pin.set_low();
 
-    thread::sleep(Duration::from_secs(1));
-
-    println!("init echo is_high: {}", echo_pin.is_high());
-
-    let pulse: Arc<Mutex<Pulse>> = Arc::new(Mutex::new(Pulse::empty()));
-    let pulse_cloned = pulse.clone();
-    echo_pin.set_async_interrupt(Trigger::Both, move |level| {
-        let instant = std::time::Instant::now();
-        if level == Level::High {
-            println!("echo raising: {:?}", instant);
-            pulse_cloned.lock().unwrap().start = Some(instant);
-        }
-        else if level == Level::Low {
-            println!("echo falling: {:?}", instant);
-            pulse_cloned.lock().unwrap().end = Some(instant);
-        }
-    })?;
-
-    // measure distance
-    trigger_pin.set_high();
-    thread::sleep(Duration::from_micros(10));
-    trigger_pin.set_low();
-
-    
-    
-    // Sleep for 10 seconds while the LED blinks.
-    thread::sleep(Duration::from_secs(1));
-    
-    echo_pin.clear_async_interrupt()?;
-    
-    let pulse_length = pulse.lock().unwrap().length();
-    
-    if let Some(duration) = pulse_length {
-        let distance = duration.as_secs_f64() * 17150_f64;
-        println!("Distance: {} cm", distance);
-    }
-    else {
-        println!("No distance measured");
-    }
+    forward2_pin.set_high();
+    backward2_pin.set_low();
 
     // wait before running motors
-    thread::sleep(Duration::from_secs(10));
-    
-    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm0 = Pwm::with_frequency(Channel::Pwm0, 100.0, 0.25, Polarity::Normal, true)?;
-    let pwm1 = Pwm::with_frequency(Channel::Pwm1, 100.0, 0.25, Polarity::Normal, true)?;
+    thread::sleep(Duration::from_secs(1));
 
-    for i in 0..=100 {
-        pwm0.set_frequency(100.0, i as f64 * 0.01f64)?;
-        pwm1.set_frequency(100.0, i as f64 * 0.01f64)?;
-        thread::sleep(Duration::from_millis(100));
-    }
-    for i in 0..=100 {
-        pwm0.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
-        pwm1.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
-        thread::sleep(Duration::from_millis(100));
-    }
+    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
+    let pwm0 = Pwm::with_frequency(Channel::Pwm0, 100.0, 1.0, Polarity::Normal, true)?;
+    let pwm1 = Pwm::with_frequency(Channel::Pwm1, 100.0, 1.0, Polarity::Normal, true)?;
+
+    thread::sleep(Duration::from_secs(1));
+
+    // for i in 0..=100 {
+    //     pwm0.set_frequency(100.0, i as f64 * 0.01f64)?;
+    //     pwm1.set_frequency(100.0, i as f64 * 0.01f64)?;
+    //     thread::sleep(Duration::from_millis(100));
+    // }
+    // for i in 0..=100 {
+    //     pwm0.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
+    //     pwm1.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
+    //     thread::sleep(Duration::from_millis(100));
+    // }
         
     Ok(())
 
