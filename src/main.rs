9bdc0bc242e1102a22d9ba34e65b6d435eddfe08
Štěpán Beneš <benes.stepan@gmail.com>
2022-05-23 17:03:50 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let gpio = rppal::gpio::Gpio::new()?;
 
-    let mut trigger_pin = gpio.get(23)?.into_output();
-    let mut echo_pin = gpio.get(24)?.into_input();
+    let mut trigger_pin = gpio.get(5)?.into_output();
+    let mut echo_pin = gpio.get(6)?.into_input();
 
     trigger_pin.set_low();
 
@@ -66,9 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     thread::sleep(Duration::from_micros(10));
     trigger_pin.set_low();
 
-    
-    
-    // Sleep for 10 seconds while the LED blinks.
+
     thread::sleep(Duration::from_secs(1));
     
     echo_pin.clear_async_interrupt()?;
@@ -83,23 +81,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("No distance measured");
     }
 
-    // wait before running motors
-    thread::sleep(Duration::from_secs(10));
+    // // wait before running motors
+    // thread::sleep(Duration::from_secs(10));
     
-    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm0 = Pwm::with_frequency(Channel::Pwm0, 100.0, 0.25, Polarity::Normal, true)?;
-    let pwm1 = Pwm::with_frequency(Channel::Pwm1, 100.0, 0.25, Polarity::Normal, true)?;
+    // // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
+    // let pwm0 = Pwm::with_frequency(Channel::Pwm0, 100.0, 0.25, Polarity::Normal, true)?;
+    // let pwm1 = Pwm::with_frequency(Channel::Pwm1, 100.0, 0.25, Polarity::Normal, true)?;
 
-    for i in 0..=100 {
-        pwm0.set_frequency(100.0, i as f64 * 0.01f64)?;
-        pwm1.set_frequency(100.0, i as f64 * 0.01f64)?;
-        thread::sleep(Duration::from_millis(100));
-    }
-    for i in 0..=100 {
-        pwm0.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
-        pwm1.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
-        thread::sleep(Duration::from_millis(100));
-    }
+    // for i in 0..=100 {
+    //     pwm0.set_frequency(100.0, i as f64 * 0.01f64)?;
+    //     pwm1.set_frequency(100.0, i as f64 * 0.01f64)?;
+    //     thread::sleep(Duration::from_millis(100));
+    // }
+    // for i in 0..=100 {
+    //     pwm0.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
+    //     pwm1.set_frequency(100.0, 1_f64 - (i as f64 * 0.01f64))?;
+    //     thread::sleep(Duration::from_millis(100));
+    // }
         
     Ok(())
 
